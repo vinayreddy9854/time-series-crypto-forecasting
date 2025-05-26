f58002675c44c17a56331f67d973022572edf1aa
vinay reddy <vinayreddy9854@gmail.com>
2025-05-26 16:20:50 +0530

    Update test_predict.py

--- test_predict.py
+++ test_predict.py
@@ -1,18 +1,10 @@
 import requests
 import json
-
-# Define the API URL
-url = "http://127.0.0.1:5000/predict"  # Adjust if you deploy to a different URL
-
-# Example features (ensure these match the model's expected input)
+url = "http://127.0.0.1:5000/predict"  
 data = {
-    'features': [0.1, 0.3, 0.4, 0.2, 0.5]  # Replace with your actual feature data
+    'features': [0.1, 0.3, 0.4, 0.2, 0.5]  
 }
-
-# Send a POST request with the data
 response = requests.post(url, json=data)
-
-# Check the response
 try:
     response_data = response.json()
     print(response_data)
